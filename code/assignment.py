095804d38620eade96d8fbc769f73a8c7c8e2aeb
norafk <noracai0625@gmail.com>
2024-03-02 18:37:18 -0500

    nionno

--- code/assignment.py
+++ code/assignment.py
@@ -147,6 +147,7 @@ class Model(tf.keras.Model):
         correct_predictions = tf.equal(tf.argmax(logits, 1), tf.argmax(labels, 1))
         return tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
 
+
 def train(model, train_inputs, train_labels):
     '''
     Trains the model on all of the inputs and labels for one epoch. You should shuffle your inputs 
@@ -158,33 +159,39 @@ def train(model, train_inputs, train_labels):
     :param train_inputs: train inputs (all inputs to use for training), 
     shape (num_inputs, width, height, num_channels)
     :param train_labels: train labels (all labels to use for training), 
-    shape (num_labels, num_classes)
-    :return: Return the average accuracy across batches of the train inputs/labels
+    shape (num_labels, num_classes)y across batches of the train inputs/labels
     '''
+    ## Compile out model with all of the required components
+    # model.compile(optimizer = model.optimizer, loss = model.loss(), metrics = model.accuracy()) ### am i supposed to do this or is this what I do manually
 
-    num_batches = len(train_inputs) // (model.batch_size) # remaining data that doens't fit into one batch - disgarded
-    indices = tf.random.shuffle(range(len(train_inputs))) # range - not just len
-    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
-    total_accuracy = 0
+    batch_size = model.batch_size
+    accuracies = []
 
-    for i in range(num_batches):
-        batch_indices = indices[i * (model.batch_size) : (i + 1) * model.batch_size] # from one batch to another
-        batch_inputs = tf.gather(train_inputs, batch_indices)
+    #shuffle inputs
+    indices = tf.random.shuffle(range(0, train_inputs.shape[0]))
+    shuffled_inputs = tf.gather(train_inputs, indices) 
+    shuffled_labels = tf.gather(train_labels, indices)
+    
+    for b, b1 in enumerate(range(batch_size, shuffled_inputs.shape[0] + 1, batch_size), 1):
+        b0 = b1 - batch_size
+        batch_inputs = shuffled_inputs[b0: b1]
         batch_inputs = tf.image.random_flip_left_right(batch_inputs)
-        batch_labels = tf.gather(train_labels, batch_indices)
+        batch_labels = shuffled_labels[b0:b1]
 
         with tf.GradientTape() as tape:
-            logits = model.call(batch_inputs) # y_pred
-            loss = model.loss(logits, batch_labels)
+            pred_labels = model.call(batch_inputs)
+            loss = model.loss(pred_labels, batch_labels)
+            model.loss_list.append(loss)
+        
+        gradients = tape.gradient(loss, model.trainable_variables) 
+        model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+        
+        batch_acc = model.accuracy(pred_labels, batch_labels)
 
-        gradients = tape.gradient(loss, model.trainable_variables)
-        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+        accuracies.append(batch_acc)
+    
+    return np.mean(accuracies)
 
-        batch_accuracy = model.accuracy(logits, batch_labels)
-        total_accuracy = total_accuracy + batch_accuracy
-
-    average_accuracy = total_accuracy / num_batches
-    return average_accuracy 
 
 def test(model, test_inputs, test_labels):
     """
@@ -198,9 +205,20 @@ def test(model, test_inputs, test_labels):
     :return: test accuracy - this should be the average accuracy across
     all batches
     """
-    logits = model(test_inputs)
-    accuracy = model.accuracy(logits, test_labels)
-    return accuracy
+
+    batch_size = model.batch_size
+    accuracies = []
+
+    for b, b1 in enumerate(range(batch_size, test_inputs.shape[0] + 1), batch_size):
+        b0 = b1 - batch_size
+        batch_inputs = test_inputs[b0: b1]
+        batch_labels = test_labels[b0:b1]
+        pred_labels = model.call(batch_inputs)
+
+        accuracies.append(model.accuracy(pred_labels, batch_labels))
+    
+    return np.mean(accuracies)
+
 
 
 def visualize_loss(losses): 
